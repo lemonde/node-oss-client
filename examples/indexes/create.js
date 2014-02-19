f3a4e217fa0cabca889f7b1d985be90be0e74f7e
Naveen.A.N <naveen.ann@gmail.com>
2014-02-19 18:59:29 +0530

    #3 Not able to use the library if OpenSearchServer has login credentials

--- examples/indexes/create.js
+++ examples/indexes/create.js
@@ -6,3 +6,16 @@ client.indexes.create('my_index', function (err, res) {
 
   console.log(res);
 });
+//Example to create OpenSearchServer client with privilege 
+var client_with_login = oss.createClient({
+  hostname: 'localhost',
+  login: 'mylogin',
+  key: 'MYKEY1811LKJA120918230129de7c77',
+  protocol: 'http'
+});
+
+client_with_login.indexes.create('my_index2', function (err, res) {
+  if (err) return console.error(err);
+
+  console.log(res);
+});
