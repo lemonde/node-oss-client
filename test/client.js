ffcddbc6b52162b3f5481c45020d89b5e826d6cd
Greg Bergé <neoziro@gmail.com>
2014-03-27 10:28:07 +0100

    Support credentials.

--- test/client.js
+++ test/client.js
@@ -13,92 +13,125 @@ describe('Client', function () {
   });
 
   describe('#request', function () {
-    var client;
+    describe('without credentials', function () {
+      var client;
 
-    beforeEach(function () {
-      client = new Client();
+      beforeEach(function () {
+        client = new Client();
 
-      nock('http://localhost:9090')
-        .post('/my-path')
-        .reply(200, {
-          foo: 'bar'
-        });
+        nock('http://localhost:9090')
+          .post('/my-path')
+          .reply(200, {
+            foo: 'bar'
+          });
 
-      nock('http://localhost:9090')
-        .post('/my-error-path')
-        .reply(400, 'My error');
+        nock('http://localhost:9090')
+          .post('/my-error-path')
+          .reply(400, 'My error');
 
-      nock('http://localhost:9090')
-        .post('/my-internal-error-path')
-        .reply(500);
+        nock('http://localhost:9090')
+          .post('/my-internal-error-path')
+          .reply(500);
 
-    });
-
-    it('should be possible to make a request', function (done) {
-
-      client.request({
-        pathname: '/my-path',
-        method: 'POST'
-      }, function (err, res) {
-        if (err) return done(err);
-
-        expect(res).to.deep.equal({
-          foo: 'bar'
-        });
-
-        done();
-      });
-    });
-
-    it('should handle error correctly', function (done) {
-
-      client.request({
-        pathname: '/my-error-path',
-        method: 'POST'
-      }, function (err, res) {
-        expect(err instanceof Error).to.be.true;
-        expect(err.message).to.be.equal('My error');
-        expect(res).to.be.undefined;
-
-        done();
-      });
-    });
-
-    it('should handle OSS internal error correctly', function (done) {
-
-      client.request({
-        pathname: '/my-internal-error-path',
-        method: 'POST'
-      }, function (err, res) {
-        expect(err instanceof Error).to.be.true;
-        expect(err.message).to.be.equal('');
-        expect(res).to.be.undefined;
-
-        done();
-      });
-    });
-
-    describe('when the server is not reachable', function() {
-
-      beforeEach(function() {
-        client = new Client({
-          hostname: 'localhost',
-          port: 99999999
-        });
       });
 
-      it('gives a usefull error message', function(done) {
+      it('should be possible to make a request', function (done) {
+
         client.request({
           pathname: '/my-path',
           method: 'POST'
-        }, function(err) {
-          var msg = 'Cannot connect to OpenSearchServer at http://localhost:99999999/my-path';
-          expect(err.message).to.equals(msg);
+        }, function (err, res) {
+          if (err) return done(err);
+
+          expect(res).to.deep.equal({
+            foo: 'bar'
+          });
+
+          done();
+        });
+      });
+
+      it('should handle error correctly', function (done) {
+
+        client.request({
+          pathname: '/my-error-path',
+          method: 'POST'
+        }, function (err, res) {
+          expect(err instanceof Error).to.be.true;
+          expect(err.message).to.be.equal('My error');
+          expect(res).to.be.undefined;
+
+          done();
+        });
+      });
+
+      it('should handle OSS internal error correctly', function (done) {
+
+        client.request({
+          pathname: '/my-internal-error-path',
+          method: 'POST'
+        }, function (err, res) {
+          expect(err instanceof Error).to.be.true;
+          expect(err.message).to.be.equal('');
+          expect(res).to.be.undefined;
+
+          done();
+        });
+      });
+
+      describe('when the server is not reachable', function() {
+
+        beforeEach(function() {
+          client = new Client({
+            hostname: 'localhost',
+            port: 99999999
+          });
+        });
+
+        it('gives a usefull error message', function(done) {
+          client.request({
+            pathname: '/my-path',
+            method: 'POST'
+          }, function(err) {
+            var msg = 'Cannot connect to OpenSearchServer at http://localhost:99999999/my-path';
+            expect(err.message).to.equals(msg);
+            done();
+          });
+        });
+      });
+    });
+
+    describe('with credentials', function () {
+      var client;
+
+      beforeEach(function () {
+        client = new Client({
+          login: 'mylogin',
+          key: 'mykey'
+        });
+
+        nock('http://localhost:9090')
+          .post('/my-path?login=mylogin&key=mykey')
+          .reply(200, {
+            foo: 'bar'
+          });
+      });
+
+      it('should be possible to make a request', function (done) {
+        client.request({
+          pathname: '/my-path',
+          method: 'POST'
+        }, function (err, res) {
+          if (err) return done(err);
+
+          expect(res).to.deep.equal({
+            foo: 'bar'
+          });
+
           done();
         });
       });
     });
-
   });
 
   describe('instances', function () {
